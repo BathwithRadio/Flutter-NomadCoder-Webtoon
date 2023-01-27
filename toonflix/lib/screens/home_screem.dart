4e96990ffa1ec6d9293a9a7994513f9e67c4ea57
유민기 <mgyou@nextree.io>
2023-01-27 21:49:43 +0800

    how to use async

--- toonflix/lib/screens/home_screem.dart
+++ toonflix/lib/screens/home_screem.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  // 2. api-service를 가져올 것이기 때문에 async
+  void waitForWebtoons() async {
+    // 3. getTodaysToons가 데이터를 가져오기 기다렸다가 받으면 webtoons에 넣음
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    // 4. setState로 refresh
+    setState(() {});
+  }
+
+  // 1. initState로 데이터를 받아오는 함수를 호출
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
